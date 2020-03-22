abc7324fc847c988b0223e8a0babae873c5b3df4
Will G Piro <willgpiro@gmail.com>
2020-03-21 18:26:15 -0700

    handle post data in server in chunks call router after recieving chunks, pass data to router, and from router to request handlers

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-        const pathname = url.parse(request.url).pathname; console.log('Request for ' + pathname + 'recieved.');
+        const postData = '';
+        const pathname = url.parse(request.url).pathname; 
+        console.log('Request for ' + pathname + 'recieved.');
 
-        route(handle, pathname, response);
+        request.setEncoding('utf8');
+
+        request.addListener('data', function(postDataChunk) {
+            postData += postDataChunk;
+            console.log('Recieved POST data chunk' + postDataChunk + '.');
+        });
+
+        request.addListener('end', function() {
+            route(handle, pathname, response);
+        });
     }
-
     http.createServer(onRequest).listen(8888);
     console.log('Server has started, Will');
 }
