5116b82496d2f2431447a3b90e51187327587626
Will G Piro <willgpiro@gmail.com>
2020-03-22 18:44:25 -0700

    html content type fixed

--- requestHandlers.js
+++ requestHandlers.js
@@ -8,19 +8,17 @@ function start(response, postData) {
 
     const body = '<html>' +
     '<head>' +
-    '<meta http-equiv="Content-Type" ' +
-    'content="text/html; charset=UTF-8" />' +
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" enctype="multipart/form-data" ' +
-    'method="post">' +
-    '<input type="file" name="upload">' +
-    '<input type="submit" value="Upload file" />' +
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload files" />' +
     '</form>' +
     '</body>' +
     '</html>';
 
-    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.writeHead(200, { "Content-Type": "text/html" });
     response.write(body);
     response.end();
 }
